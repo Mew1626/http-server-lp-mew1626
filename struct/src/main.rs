3e9b6d02e3cde6649ed5154ca0f9e3eb3d4f8ed8
Avery Clary <2.clary.avery@gmail.com>
2023-12-28 23:51:31 -0600

    4. Complex Data Structures

--- struct/src/main.rs
+++ struct/src/main.rs
@@ -1,11 +1,16 @@
-#[derive(Debug)]
+#[derive(Debug, PartialEq, Clone)]
 enum Lang {
   English,
   Spanish,
   Chinese,
   Texan,
+  Icelandic,
+  Arabic,
+  Ukrainian,
+  German,
 }
 
+#[derive(Clone)]
 struct Greeting {
     message: String,
     lang: Lang,
@@ -22,8 +27,35 @@ fn main() {
   v.push(g);
   let g : Greeting = Greeting { lang: Lang::Chinese, message: String::from("WasmEdge 你好!") };
   v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Icelandic, message: String::from("Halló WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Arabic, message: String::from("!WasmEdge مرحبًا") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::Ukrainian, message: String::from("Привіт WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::German, message: String::from("Hallo WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::German, message: String::from("Guten Tag WasmEdge!") };
+  v.push(g);
+  let g : Greeting = Greeting { lang: Lang::German, message: String::from("Guten Abend WasmEdge!") };
+  v.push(g);
+
+  let cloned_v: Vec<Greeting>  = v.clone();
 
   for e in v {
     println!("{:?} {}", e.lang, e.message);
   }
+
+  let query = Lang::Icelandic;
+  let m: Greeting = cloned_v.clone().into_iter().find(|g| g.lang == query).unwrap();
+  println!("\nQuery: {:?} Response: {}", query, m.message);
+  
+  let query = Lang::German;
+  let m: Vec<Greeting> = cloned_v.into_iter().filter(|g| g.lang == query).collect();
+  println!("\nQuery: {:?}", query);
+  for e in m{
+    println!("Greeting: {}", e.message);
+  }
+
+
 }
